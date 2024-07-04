7b766b9973a1c49fdc05022820f0d0331aef7e2e
Rilax <maxou.fe@gmail.com>
2024-07-04 13:45:41 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,12 +1,13 @@
-# Information configure sur https://console.cloud.google.com/ --> Client ID for Web application :
+# Information to configure on https://console.cloud.google.com/ --> Client ID for Web application :
 $clientId = "" # OAuth 2.0 Client ID
 $clientSecret = "" # OAuth 2.0 Client Secret
 $redirecturi = "http://localhost" # OAuth 2.0 Authorized redirect URIs
 $uri = "https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fphotoslibrary.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=$redirecturi&client_id=$clientid"
-# Le code de retour dans l'url de retour apres avoir montrer patte blanche avec son compte Google à la precedente url $uri :) :
+# The return code in the return url after showing your credentials with your Google account at the previous url $uri :) :
 $code = ""
+$filepath = "C:\temp\Backup_Google_Photos" # Example of a directory for storing your downloading images/videos
 
-# creation de la requete de code :) :
+# Creating the code query
 $param = @{
     Uri         = "https://oauth2.googleapis.com/token"
     Method      = "Post"
@@ -21,28 +22,25 @@ $param = @{
 }
 $token = Invoke-RestMethod @param
 
-# generation du Headers d'authentification :
+# Generation of the authentication headers
 $headers = @{"Authorization" = "Bearer $($token.access_token)"}
-# On stock le refreshtoken :
+# Store the refresh_token
 $refreshToken = $token.refresh_token
 
-# On prepare la fonction de refreshToken :
+# Fonction for creating a new token with the refreshToken
 function Refresh-AccessToken {
     param (
         [string]$clientId,
         [string]$clientSecret,
         [string]$refreshToken
     )
-
     $body = @{
         client_id = $clientId
         client_secret = $clientSecret
         refresh_token = $refreshToken
         grant_type = 'refresh_token'
     }
-
     $response = Invoke-RestMethod -Uri 'https://oauth2.googleapis.com/token' -Method Post -ContentType 'application/x-www-form-urlencoded' -Body $body
-
     if ($response.access_token) {
         return $response
     } else {
@@ -50,18 +48,13 @@ function Refresh-AccessToken {
     }
 }
 
-<# Good :) il fallait bien debuter quelque part :) :
-$uri = "https://photoslibrary.googleapis.com/v1/mediaItems"
-$result = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
-#>
-
 $url = 'https://photoslibrary.googleapis.com/v1/mediaItems'
-$pageSize = 100  # Nombre d'éléments par page, peut aller jusqu'à 100
+$pageSize = 100  # Number of items per page, can be up to 100
 
-# Initialiser le token de page
+# Initialize the page token
 $pageToken = $null
 
-# Boucle pour récupérer toutes les pages Powershell 5.1
+### Loop to recover all images/videos only with Powershell 5.x but take very long time ###
 do {
     $queryParams = @{
         pageSize = $pageSize
@@ -70,37 +63,37 @@ do {
         $queryParams.pageToken = $pageToken
     }
 
-    # Construire l'URL avec les paramètres de requête
+    # Construct the URL with the query parameters
     $queryString = ($queryParams.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join '&'
     $fullUrl = "$url`?$queryString"
 
-    # Effectuer la requête GET
+    # Make the GET request
     $response = Invoke-RestMethod -Uri $fullUrl -Headers $headers -Method Get
     # Mise en cache de la page suivante
     $pageToken = $response.nextPageToken
 
-    # Vérifiez si la requête a réussi
+    # Checks if the query was successful
     if ($response -and $response.mediaItems) {
         $mediaItems = $response.mediaItems
     } else {
         Write-Output "No more media items."
     }
 
-    # Téléchargement...
+    # Downloading...
     try {
         foreach ($item in $mediaItems) {
-            # Récupérer les informations sur l'image
+            # Retrieve image information
             $baseUrl = $item.baseUrl  # URL de base de l'image
             $filename = "$(Get-Date $item.mediaMetadata.creationTime -Format yyyy-MM-dd_hh-mm-ss).$(($item.filename -split "\.")[-1])"  # Nom du fichier
             
-            # URL de téléchargement en fonction d'une image ou d'une vidéo :
+            # Creating the download URL based on an image or video
             if ($item.mimeType -like "video*") {
                 $imageUrl = "$baseUrl=dv"
             } else {
                 $imageUrl = "$baseUrl=d"
             }
             
-            # Télécharger l'image en utilisant l'URL de base (baseUrl)
+            # Download the image using the base URL (baseUrl)
             $outputFile = "C:\temp\Backup_Google_Photos\$filename"
             Invoke-WebRequest -Uri $imageUrl -OutFile $outputFile
         }
@@ -114,12 +107,12 @@ do {
             }
             $refreshToken = $accesstoken.refresh_token
         } else {
-            New-Item -Path "C:\temp\Backup_Google_Photos\error" -Name "$($error[0].Exception.Response.StatusCode.Value__)_$($filename).txt" -ItemType "file" -Value $item
+            New-Item -Path "$filepath\error" -Name "$($error[0].Exception.Response.StatusCode.Value__)_$($filename).txt" -ItemType "file" -Value $item
         }
     }
 } while ($pageToken)
 
-# Boucle pour récupérer toutes les pages Powershell 7.4
+### Loop to retrieve all images/videos faster than Powershell 5.x but requires Powershell 7.x ###
 $batchSize = 10  # Nombre de téléchargements parallèles
 do {
     $queryParams = @{
@@ -129,37 +122,37 @@ do {
         $queryParams.pageToken = $pageToken
     }
 
-    # Construire l'URL avec les paramètres de requête
+    # Construct the URL with the query parameters
     $queryString = ($queryParams.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join '&'
     $fullUrl = "$url`?$queryString"
 
-    # Effectuer la requête GET
+    # Make the GET request
     $response = Invoke-RestMethod -Uri $fullUrl -Headers $headers -Method Get
-    # Mise en cache de la page suivante
+    # Caching the next page
     $pageToken = $response.nextPageToken
 
-    # Vérifiez si la requête a réussi
+    # Checks if the query was successful
     if ($response -and $response.mediaItems) {
         $mediaItems = $response.mediaItems
     } else {
         Write-Output "No more media items."
     }
 
-    # Téléchargement...
+    # Downloading...
     try {
         $mediaItems | ForEach-Object -ThrottleLimit $batchSize -Parallel {
             $baseUrl = $_.baseUrl  # URL de base de l'image
             $filename = "$(Get-Date $_.mediaMetadata.creationTime -Format yyyy-MM-dd_hh-mm-ss).$(($_.filename -split "\.")[-1])"  # Nom du fichier
             
-            # URL de téléchargement en fonction d'une image ou d'une vidéo :
+            # Creating the download URL based on an image or video
             if ($_.mimeType -like "video*") {
                 $imageUrl = "$baseUrl=dv"
             } else {
                 $imageUrl = "$baseUrl=d"
             }
             
-            # Télécharger l'image en utilisant l'URL de base (baseUrl)
-            $outputFile = "C:\temp\Backup_Google_Photos\$filename"
+            # Download the image using the base URL (baseUrl)
+            $outputFile = "$FilePath\$filename"
             Invoke-WebRequest -Uri $imageUrl -OutFile $outputFile
         }
     } catch {
@@ -172,7 +165,7 @@ do {
             }
             $refreshToken = $accesstoken.refresh_token
         } else {
-            New-Item -Path "C:\temp\Backup_Google_Photos\error" -Name "$($error[0].Exception.Response.StatusCode.Value__)_$($filename).txt" -ItemType "file" -Value $item
+            New-Item -Path "$filepath\error" -Name "$($error[0].Exception.Response.StatusCode.Value__)_$($filename).txt" -ItemType "file" -Value $item
         }
     }
 } while ($pageToken)
